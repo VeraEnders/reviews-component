00bb64e117752be6286181dd5ca2bf2d1930fa0f
VeraEnders <veraabramo@gmail.com>
2024-02-04 17:13:04 +0100

    add event listeners for tags and search input

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const tags = document.querySelectorAll('#tag')
+const search = document.querySelector('#search')
+
+tags.forEach(tag => tag.addEventListener('click', function(e) {
+  const clicked = e.target;
+  tags.forEach(tag => tag.classList.remove('bg-[#8781f9]'));
+  tags.forEach(tag => tag.classList.remove('text-white'))
+
+  clicked.classList.toggle('bg-[#8781f9]');
+  clicked.classList.toggle('text-white');
+
+  const clickedText = e.target.textContent;
+
+  search.value = `${clickedText}`;
+  search.classList.add('text-white');
+}));
+
+search.addEventListener('click', function() {
+  search.classList.remove('text-white');
+})
